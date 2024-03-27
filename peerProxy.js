a44c41b2ffa7d2eb2fb13b37d7fea80cacbfc4bd
radmuffin <70724879+radmuffin@users.noreply.github.com>
2024-03-26 19:59:53 -0600

    created peerProxy

---
+++ peerProxy.js
@@ -0,0 +1,13 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
+function peerProxy(httpServer) {
+  // Create a websocket object
+  const wss = new WebSocketServer({ noServer: true });
+
+  // Handle the protocol upgrade from HTTP to WebSocket
+  httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
